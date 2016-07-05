51072aba8ecd3241826027172b4a2b9a8e64642a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-07-05 13:40:15 -0400

    fix face background and foreground

--- setup/setup-faces.el
+++ setup/setup-faces.el
@@ -1,5 +1,8 @@
-(setq background-color "#404040") ;; gray25
-(setq foreground-color "#ffec8b") ;; LightGoldenrod1
+(setq default-frame-alist
+      '((background-color . "#404040") ;; gray25
+        (foreground-color . "#eedc82") ;; LightGoldenrod2
+        )
+      )
 
 (set-face-attribute 'default nil :height 90)
 
