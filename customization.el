2d794082b4e031b75a09c4b0767c71b01e16a79a
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-10-26 11:21:16 -0400

    change faces

--- customization.el
+++ customization.el
@@ -1,1 +1,18 @@
 (message "Loading customization file")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "orange red"))))
+ '(font-lock-constant-face ((t (:foreground "green yellow"))))
+ '(font-lock-function-name-face ((t (:foreground "dark turquoise"))))
+ '(font-lock-keyword-face ((t (:foreground "goldenrod"))))
+ '(font-lock-type-face ((t (:foreground "orchid"))))
+ '(font-lock-variable-name-face ((t (:foreground "chocolate")))))
