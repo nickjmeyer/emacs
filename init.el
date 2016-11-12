2aec79f2e5d0fb529bdfa78e162202f6a0464379
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-11-12 16:14:45 -0500

    update melpa repo

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
 
