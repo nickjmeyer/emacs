60fb0da602cec1a3dfe973e34022b5c0b1a573c2
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-12 11:38:01 -0500

    revert back to M-n and M-p for company scrolling

--- init.el
+++ init.el
@@ -113,11 +113,11 @@
 (define-key c++-mode-map  [(control tab)] 'company-complete)
 (define-key c-mode-map  (kbd "C-TAB") 'company-complete)
 (define-key c++-mode-map  (kbd "C-TAB") 'company-complete)
-(with-eval-after-load 'company
-  (define-key company-active-map (kbd "M-n") nil)
-  (define-key company-active-map (kbd "M-p") nil)
-  (define-key company-active-map (kbd "C-n") #'company-select-next)
-  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+;; (with-eval-after-load 'company
+;;   (define-key company-active-map (kbd "M-n") nil)
+;;   (define-key company-active-map (kbd "M-p") nil)
+;;   (define-key company-active-map (kbd "C-n") 'company-select-next)
+;;   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 ;; (define-key c-mode-map  [(control tab)] 'company-complete)
