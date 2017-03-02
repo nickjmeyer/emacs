b66f3943cb15cbd94aa5fa7302d145f7ca2864b4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-03-02 00:09:32 -0500

    edit faces

--- customization.el
+++ customization.el
@@ -12,9 +12,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:foreground "orange red"))))
- '(font-lock-constant-face ((t (:foreground "green yellow"))))
- '(font-lock-function-name-face ((t (:foreground "dark turquoise"))))
- '(font-lock-keyword-face ((t (:foreground "goldenrod"))))
- '(font-lock-type-face ((t (:foreground "orchid"))))
+ '(default ((t (:inherit nil :stipple nil :background "midnight blue" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Menlo"))))
+ '(company-scrollbar-bg ((t (:background "alternateSelectedControlColor"))))
+ '(company-scrollbar-fg ((t (:background "dark slate blue"))))
+ '(company-tooltip ((t (:background "MediumPurple4" :foreground "gray60"))))
+ '(company-tooltip-annotation ((t (:foreground "DeepSkyBlue2"))))
+ '(company-tooltip-common ((t (:foreground "chartreuse4"))))
+ '(company-tooltip-selection ((t (:background "tomato4"))))
+ '(font-lock-comment-face ((t (:foreground "red1"))))
+ '(font-lock-constant-face ((t (:foreground "yellow1"))))
+ '(font-lock-function-name-face ((t (:foreground "cyan3"))))
+ '(font-lock-keyword-face ((t (:foreground "chartreuse2"))))
+ '(font-lock-preprocessor-face ((t (:inherit font-lock-builtin-face :foreground "MediumPurple1"))))
+ '(font-lock-string-face ((t (:foreground "HotPink1"))))
+ '(font-lock-type-face ((t (:foreground "orange2"))))
  '(font-lock-variable-name-face ((t (:foreground "chocolate")))))
