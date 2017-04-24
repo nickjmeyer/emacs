9f17720a6c2f2d86a5eb8e904c688453a6386312
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-04-24 18:47:55 -0400

    add org mode package repo

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 (require 'package)
 (add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/")
              '("melpa" . "http://melpa.org/packages/"))
+
 (package-initialize)
 (package-refresh-contents)
 
