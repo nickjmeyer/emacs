1ea472fa7df665a99f0a8c9451fbfa9873bbde30
Nick Meyer <nick.j.meyer@gmail.com>
2023-07-18 09:59:05 -0400

    Updates

--- customization.el
+++ customization.el
@@ -15,5 +15,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "color-234" :foreground "#dfdfdf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 1 :width normal :foundry "default" :family "default"))))
  '(ivy-current-match ((((class color) (background light)) :background "blue" :foreground "white") (((class color) (background dark)) :background "blue" :foreground "black")))
- '(ivy-cursor ((t (:box (:line-width 2 :color "lime green"))))))
+ '(ivy-cursor ((t (:box (:line-width 2 :color "lime green")))))
+ '(magit-diff-added ((t (:extend t :background "color-22" :foreground "color-41"))))
+ '(magit-diff-added-highlight ((t (:extend t :background "color-22" :foreground "color-41" :weight bold))))
+ '(vertical-border ((t (:background "color-238" :foreground "color-232")))))
