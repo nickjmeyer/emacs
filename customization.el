05b526e62a82b27cbafa1a7724e0cab47bca4588
Nick Meyer <nick.j.meyer@gmail.com>
2023-10-31 17:15:43 +0000

    Fix magit diff colors.

--- customization.el
+++ customization.el
@@ -8,12 +8,13 @@
  '(ivy-count-format "(%d/%d) ")
  '(ivy-mode t)
  '(ivy-use-virtual-buffers t)
- '(package-selected-packages
-   '(elpy cl yaml-mode use-package undo-tree rust-mode python-docstring multi-term modern-cpp-font-lock markdown-mode json-mode hydra groovy-mode gnu-elpa-keyring-update git-gutter-fringe flycheck-yamllint flx eyebrowse exec-path-from-shell evil-mc evil-magit evil-collection ess doom-themes doom-modeline diminish counsel-projectile company cmake-mode bazel-mode)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "gray12" :foreground "#dfdfdf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 100 :width normal))))
- '(ivy-current-match ((((class color) (background light)) :background "blue" :foreground "white") (((class color) (background dark)) :background "blue" :foreground "black"))))
+ '(ivy-current-match ((((class color) (background light)) :background "blue" :foreground "white") (((class color) (background dark)) :background "blue" :foreground "black")))
+ '(magit-diff-added ((t (:extend t :background "color-22"))))
+ '(magit-diff-added-highlight ((t (:extend t :background "color-22" :foreground "#9ac334" :weight bold)))))
